934febf9b9d30c923c09fbed3b2be5d06b641feb
Sergey Blashkov <sblashkov@gmail.com>
2026-09-12 15:03:19 -0700

    release: Bump version to 3.0 (21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.sblashkov.yesnorandomizer"
     minSdk = 24
     targetSdk = 37
-    versionCode = 20
-    versionName = "2.0"
+    versionCode = 21
+    versionName = "3.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
